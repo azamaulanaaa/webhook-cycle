5282eec9f6ca99c604704c7b22e3222430e4c538
Aza Maulana <azamaulanaaa@gmail.com>
2025-10-24 16:01:03 +0700

    feat: add logger

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,35 @@
+use anyhow::Context;
 use clap::Parser;
+use simple_logger::SimpleLogger;
 
 #[derive(clap::Parser, Debug)]
 #[command(version)]
-struct Args {}
+struct Args {
+    #[arg(long, default_value_t = false, help = "Set logger to debug mode")]
+    verbose: bool,
+}
 
 fn main() -> anyhow::Result<()> {
     let args = Args::try_parse()?;
 
-    println!("Hello, world!");
+    init_logger(args.verbose)?;
+
+    log::info!("Hello, world!");
+
+    Ok(())
+}
+
+fn init_logger(verbose: bool) -> anyhow::Result<()> {
+    let log_level = if verbose {
+        log::LevelFilter::Debug
+    } else {
+        log::LevelFilter::Info
+    };
+
+    SimpleLogger::new()
+        .with_level(log_level)
+        .init()
+        .context("Failed to initiate logger")?;
 
     Ok(())
 }
