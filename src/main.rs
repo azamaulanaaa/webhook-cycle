fdf7fa3698a29a3289300876dd6dc27c1ab1dee3
Aza Maulana <azamaulanaaa@gmail.com>
2025-10-24 15:56:37 +0700

    feat: add args parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
+use clap::Parser;
+
+#[derive(clap::Parser, Debug)]
+#[command(version)]
+struct Args {}
+
+fn main() -> anyhow::Result<()> {
+    let args = Args::try_parse()?;
+
     println!("Hello, world!");
+
+    Ok(())
 }
