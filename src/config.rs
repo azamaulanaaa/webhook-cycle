fcad0d6830be07e1404ae28d8060903f75db639b
Aza Maulana <azamaulanaaa@gmail.com>
2025-10-24 16:32:15 +0700

    feat(config): add validation for 'is file`

--- src/config.rs
+++ src/config.rs
@@ -12,6 +12,13 @@ impl TryFrom<&Path> for Config {
     type Error = anyhow::Error;
 
     fn try_from(value: &Path) -> Result<Self, Self::Error> {
+        if !value.is_file() {
+            return Err(anyhow::anyhow!(
+                "Given path '{:?}' is not a file",
+                value.to_str()
+            ));
+        }
+
         let content = fs::read_to_string(value).context("Failed to read config file")?;
         let config: Self = toml::from_str(&content).context("Failed to parse config file")?;
 
