afd2c1b4c1d7839050cb011b6d7b739243535993
Aza Maulana <azamaulanaaa@gmail.com>
2025-10-24 16:17:18 +0700

    feat: add http server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::net::{SocketAddrV4, TcpListener};
+
+use actix_web::{App, HttpServer};
 use anyhow::Context;
 use clap::Parser;
 use simple_logger::SimpleLogger;
@@ -15,7 +18,19 @@ async fn main() -> anyhow::Result<()> {
 
     init_logger(args.verbose)?;
 
-    log::info!("Hello, world!");
+    let listening_addr = "0.0.0.0:8080";
+    let listening_addr: SocketAddrV4 = listening_addr
+        .parse()
+        .context("Failed to parse listening addr")?;
+    let listener =
+        TcpListener::bind(listening_addr).context("Failed to start listening at given address")?;
+
+    HttpServer::new(move || App::new())
+        .listen(listener)
+        .context("Http server cannot start listening")?
+        .run()
+        .await
+        .context("Http server is crash")?;
 
     Ok(())
 }
